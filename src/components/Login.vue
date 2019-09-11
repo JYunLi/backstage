1d6cb88c28b0f9dbc6f54e51b4f589f017dcbe60
jyl <892934306@qq.com>
2019-09-11 17:05:41 +0800

    第一次上传，登录只做了一点点

---
+++ src/components/Login.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="login_container">
+        <!-- 这是登录页面class      -->
+        <div class="login_box">
+        </div>
+        <dir class="login_icon">
+
+        </dir>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+    .login_container{
+        height: 100%;
+        // background: #2b4b6b;
+    };
+    .login_box {
+      width: 450px;
+      height: 300px;
+      background-color: #fff;
+      border-radius: 8px;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+            
+    }
+</style>
